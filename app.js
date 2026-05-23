d5dd29cf0fcee88d81b602216ec708bbbc340362
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-05-24 00:02:09 +0530

    created express server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.send("Hello World!")
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is up and running on port 3000! Ready to handle requests.");
+});
