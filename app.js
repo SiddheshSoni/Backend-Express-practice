d51414f842151b2a964e4a71efb0c22755d03dc3
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-05-24 00:33:08 +0530

    middlewares task

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send("Hello World!")
-});
+app.get("/", (req, res)=>{
+    res.send("Home Page!");
+})
+
+const customMW = (req, res, next)=>{
+    req.user = "Guest";
+    next();
+};
+
+app.get("/welcome", customMW, (req, res)=>{
+    res.send(`<h1> Welcome, ${req.user} </h1>`)
+})
 
 app.listen(3000, ()=>{
     console.log("Server is up and running on port 3000! Ready to handle requests.");
